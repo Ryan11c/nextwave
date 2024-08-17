17eeae9126422f81f7c7ad5fa6257f5f87193990
Ryan11c <ryan04122005@gmail.com>
2024-08-16 21:55:45 -0700

    added footer

--- homeapp.js
+++ homeapp.js
@@ -11,3 +11,32 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(`.hidden`)
 hiddenElements.forEach((el) => observer.observe(el));
+
+/*Form submission*/
+document.getElementById('btn-submit').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const firstName = document.getElementById('input-name').value;
+    const email = document.getElementById('input-email').value;
+    const message = document.getElementById('input-message').value;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Port: 2525,
+        Username : "nextwavecodingacademy@gmail.com",
+        Password : "56B3964E798B5FD3FE10356F42B1CD4150B6",
+        To : 'ryan04122005@gmail.com',
+        From : "ryan04122005@gmail.com",
+        Subject : "New Join us form submission",
+        Body: `
+            <strong>Name:</strong> ${firstName}<br>
+            <strong>Email:</strong> ${email}<br>
+            <strong>Message:</strong> ${message}
+        `
+    }).then(message => alert('Form submitted successfully!'));
+
+    // Reset the form after submission
+    document.getElementById('input-name').value = '';
+    document.getElementById('input-email').value = '';
+    document.getElementById('input-message').value = '';
+});
